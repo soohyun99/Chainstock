a131f997437c1627f0f8d9d136f4f720f84fabd4
seungwon2 <dandelionsw2@ewhain.net>
2021-11-10 23:12:04 +0900

    :art: [Chore] : apply styled-component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+/** @format */
+import React from "react";
+import Head from "next/head";
+import styled, { createGlobalStyle } from "styled-components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>StockChain</title>
+			</Head>
+			<GlobalStyle />
+			<Main>
+				<Component {...pageProps} />
+			</Main>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
+
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
+const Main = styled.main`
+	width: 100vw;
+	height: 100vh;
+`;
